4154711f05c0e012791e2a9f0fede020996033d0
mah454 <mahsom@gmail.com>
2012-07-04 15:58:12 +0430

    Update master

--- mkmessage.sh
+++ mkmessage.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 
 
-for (( i=0 ; i < 2 ; i++ )) ; do 
-		#echo "22308230" | mutt -a file.pdf -s 'test message' -- fax@localhost 		
-		echo "22308230" | mutt -a file.pdf -s 'test message' -- mahsom@gmail.com
+for (( i=0 ; i < 2 ; i++ )) ; do 	
+		echo "22308230" | mutt -a file.pdf -s 'test message' -- NAME@example.info
 done
