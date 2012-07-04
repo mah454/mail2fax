7677795d9e7b09f8ad2bded48ab59a375041b64c
Mahdi Sheikh hossini <mahsom@gmail.com>
2012-07-04 16:24:35 +0430

    Mail2Fax bash script

--- mkmessage.sh
+++ mkmessage.sh
@@ -2,6 +2,5 @@
 
 
 for (( i=0 ; i < 2 ; i++ )) ; do 
-		#echo "22308230" | mutt -a file.pdf -s 'test message' -- fax@localhost 		
-		echo "22308230" | mutt -a file.pdf -s 'test message' -- mahsom@gmail.com
+		echo "22308230" | mutt -a file.pdf -s 'test message' -- USERNAME@example.info
 done
